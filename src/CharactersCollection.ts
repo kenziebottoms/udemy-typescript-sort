7662b3db7fd0b2c609de5a1fd4cf5c0cfbdd80ec
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-30 11:20:54 -0500

    char collection

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,25 @@
+import { Sortable } from './Sorter';
+
+export class CharactersCollection implements Sortable {
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const charArray = this.data.split('');
+
+    const leftHand = charArray[leftIndex];
+    charArray[leftIndex] = charArray[rightIndex];
+    charArray[rightIndex] = leftHand;
+
+    this.data = charArray.join('');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const sorter = new Sorter(new NumbersCollection([3, 2, 1]));
 sorter.sort();
 console.log(sorter.collection);
+
+const charSorter = new Sorter(new CharactersCollection('asdf'));
+charSorter.sort();
+console.log(charSorter.collection);
