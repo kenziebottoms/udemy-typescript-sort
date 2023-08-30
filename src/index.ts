4e98f44e5ed1f132230824dd1dbe1b0a66da96f4
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-30 11:51:16 -0500

    abstract class sorter

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 
-const sorter = new Sorter(new NumbersCollection([3, 2, 1]));
-sorter.sort();
-console.log(sorter.collection);
+const numbers = new NumbersCollection([3, 2, 1]);
+numbers.sort();
+console.log(numbers.data);
 
-const charSorter = new Sorter(new CharactersCollection('asdf'));
-charSorter.sort();
-console.log(charSorter.collection);
+const chars = new CharactersCollection('asdf');
+chars.sort();
+console.log(chars.data);
 
 const linkedList = new LinkedList();
 linkedList.add(5);
 linkedList.add(3);
 linkedList.add(1);
 
-const linkSorter = new Sorter(linkedList);
-linkSorter.sort();
+linkedList.sort();
 linkedList.print();
